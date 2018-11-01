4256d7a1fef6a6a3300f613b51ef1bdc0cdd2015
Kristijan Bobevski <kristijan_bobevski@hotmail.com>
2018-11-01 12:39:51 +0100

    Updates

---
+++ precache-manifest.68fd21bde5f2c1a7b0e0283e11c7fece.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "09871e20c30ff3ce6b175593a6f714b6",
+    "url": "/gametime/static/media/03.09871e20.png"
+  },
+  {
+    "revision": "686f26036b4b3b6cc6c2",
+    "url": "/gametime/static/css/main.2269c1f7.chunk.css"
+  },
+  {
+    "revision": "3aa23149b6cdb23bff95",
+    "url": "/gametime/static/js/1.3aa23149.chunk.js"
+  },
+  {
+    "revision": "517e8416d24d5de23ed9",
+    "url": "/gametime/static/js/runtime~main.517e8416.js"
+  },
+  {
+    "revision": "b48fe09b148f195f47439945f9d2cd35",
+    "url": "/gametime/static/media/01.b48fe09b.png"
+  },
+  {
+    "revision": "aa1ba4eb7bf9ed84d97317d0264f1bc8",
+    "url": "/gametime/static/media/02.aa1ba4eb.png"
+  },
+  {
+    "revision": "686f26036b4b3b6cc6c2",
+    "url": "/gametime/static/js/main.686f2603.chunk.js"
+  },
+  {
+    "revision": "0b078d6550da23b92e4befd442f8eca9",
+    "url": "/gametime/static/media/04.0b078d65.png"
+  },
+  {
+    "revision": "59de13bfc0f2bd3b00529610c0f56801",
+    "url": "/gametime/static/media/05.59de13bf.png"
+  },
+  {
+    "revision": "6a9fb2ed267819dd5076b8865f849eeb",
+    "url": "/gametime/static/media/instagram.6a9fb2ed.png"
+  },
+  {
+    "revision": "9a513524f2be4401637c4fee6191cd2e",
+    "url": "/gametime/static/media/twitter.9a513524.png"
+  },
+  {
+    "revision": "764b7646cfda0221e3fb4a85a27861ef",
+    "url": "/gametime/index.html"
+  }
+];
