81f3bbe5a8ed65271e0b05eb21a54bfa0dc1b15c
Kristijan Bobevski <kristijan_bobevski@hotmail.com>
2018-11-01 13:54:46 +0100

    switched the json

--- src/Components/Games.js
+++ src/Components/Games.js
@@ -19,7 +19,7 @@ class Games extends Component {
 	}
 
 	async componentDidMount(){
-		let games = await axios.get("http://127.0.0.1:3001/games");
+		let games = await axios.get("https://bobevsky.github.io/db/db.json/games");
 
 		this.setState({
 			games: games.data,
