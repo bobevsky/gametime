2684f88f3c7ce1fd66633bc7d4967cce3168809c
Kristijan Bobevski <kristijan_bobevski@hotmail.com>
2018-11-14 23:42:39 +0100

    fixed showing picture, when there is no any

--- src/Components/Gamepage.js
+++ src/Components/Gamepage.js
@@ -9,16 +9,14 @@ import {Helmet} from "react-helmet";
 class Gamepage extends Component {
 	state = {
 		games: [],
-		steps: [],
-		images: []
+		steps: []
 	}
 
 	async componentDidMount(){
 		let games = await axios.get(`https://json-project3-server.herokuapp.com/posts/${this.props.match.params.id}`);
 		this.setState({
 			games: games.data,
-			steps: games.data.steps,
-			images: games.data.steps.stepImg
+			steps: games.data.steps
 		})
 	}
 	
